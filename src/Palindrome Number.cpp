03da3a9bd609de92db1e58a4d532302ccb49a2c1
Gong Lingpu <409349942@qq.com>
2015-02-10 18:32:55 +0800

    palindrome number

--- src/Palindrome Number.cpp
+++ src/Palindrome Number.cpp
@@ -3,6 +3,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        if(x < 0)
+			return false;
+		else{
+			int a1=x,a2=0;
+			while(a1 != 0){
+				a2 = a2 * 10 + a1 %10;
+				a1 = a1 /10;
+			}
+			if(a2 == x)
+				return true;
+		}
+		return false;
+
     }
 };
