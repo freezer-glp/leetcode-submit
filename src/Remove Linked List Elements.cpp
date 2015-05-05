940fe74944995c047a18f44950120d170ab25af9
Gong Lingpu <409349942@qq.com>
2015-05-06 00:09:43 +0800

    Remove Linked List Elements

---
+++ src/Remove Linked List Elements.cpp
@@ -0,0 +1,26 @@
+/*Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5*/
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL)
+            return NULL;
+        while(head != NULL && head -> val == val)
+			head = head -> next;
+		ListNode* cptr = head;
+		
+		while(cptr != NULL){
+		    if(cptr -> next == NULL)
+		        return head;
+			if( cptr -> next -> val == val){
+				cptr -> next = cptr -> next ->next;
+			}else{
+				cptr = cptr -> next;
+			}
+		}
+		return head;
+    }
+};
